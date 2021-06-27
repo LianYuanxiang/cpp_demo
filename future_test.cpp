00d4def19f8c685a6d98683dd837b9e650cecd7f
lianyuanxiang <lianyuanxiang@jd.com>
2021-06-27 17:31:40 +0800

    future test

--- future_test.cpp
+++ future_test.cpp
@@ -4,8 +4,9 @@
 
 // future提供获取异步调用结果的途径
 int main() {
+    // std::packaged_task 的模板参数为要封装函数的类型
     std::packaged_task<int()> task([](){return 7;});
-    std::future<int> result = task.get_future();
+    std::future<int> result = task.get_future();  // 在一个线程中执行 task
     std::thread(std::move(task)).detach();
     std::cout << "waiting..." << std::endl;
     result.wait();  // 设置barrier，阻塞future的完成
